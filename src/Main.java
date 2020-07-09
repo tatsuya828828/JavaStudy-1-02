b35dc1c150ccf56b12286be491550095eaeb36ba
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 13:16:36 +0900

    [finish]02-04

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
 	public static void main(String[] args) {
-		// エスケープシーケンスは環境によっては＼だったり¥だったりする
-		System.out.println("私の好きな記号は二重引用符 (\") です");
+		int a;
+		a = 100;
+		a++;
+		System.out.println(a);
 	}
 }
